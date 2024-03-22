a4be2d85a03c89627d81bc1f557eaac07232a8c2
Rodolfo Oliveira <dorfusouza@gmail.com>
2024-03-22 15:06:45 -0300

    modified:   .gitignore 	modified:   API/API.csproj 	new file:   API/Controllers/UsuariosController.cs 	new file:   API/Models/Evento.cs 	modified:   API/Models/Usuario.cs

--- API/Models/Usuario.cs
+++ API/Models/Usuario.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,25 @@ namespace API.Models
 {
     public class Usuario
     {
+        [Column("id_usuario")]
         public int IdUsuario { get; set; }
+
+        [Column("nome_completo")]
         public string NomeCompleto { get; set; }
+
+        [Column("email")]
         public string? Email { get; set; }
+
+        [Column("nome_completo")]
         public string Senha { get; set; }
+
+        [Column("telefone")]
         public string Telefone { get; set; }
+
+        [Column("perfil")]
         public string perfil { get; set; }
+
+        [Column("status")]
         public int Status { get; set; }
     }
 }
